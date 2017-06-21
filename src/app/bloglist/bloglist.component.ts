c8d0b4b1d6185af17b33bb64a08011ef88fb4375
codingpajamas <nanjiro.ko@gmail.com>
2017-06-21 22:45:20 +0800

    update href to routerlink, disable button while loading page, fix current page computation

--- src/app/bloglist/bloglist.component.ts
+++ src/app/bloglist/bloglist.component.ts
@@ -12,9 +12,11 @@ export class BloglistComponent implements OnInit {
   blogs: Blog[];
   currentPage:number;
   totalPages:number;
+  isLoading:boolean;
 
   constructor(private blogService: BlogService, private router: Router, private route: ActivatedRoute) {
   	this.totalPages = 1;
+    this.isLoading = false;
 
     this.route.queryParams.subscribe(params => {
       this.currentPage = params['page'] || 1;
@@ -27,23 +29,33 @@ export class BloglistComponent implements OnInit {
   		.then(blogs => {
   			this.blogs = blogs;
   			this.totalPages = this.blogService.totalPages;
+        this.isLoading = false;
   		});
   } 
 
   getNextBlogs(): boolean {
-    this.currentPage +=this.currentPage;
+    this.isLoading = true;
+    this.currentPage = Number(this.currentPage) + 1;
     this.getBlogs();
+    this.router.navigateByUrl(`blog?page=${this.currentPage}`);
     return false;
   } 
 
   getPreviousBlogs(): boolean {
-    this.currentPage = this.currentPage - 1;
+    this.isLoading = true;
+    this.currentPage = Number(this.currentPage) - 1;
     this.getBlogs();
-
+    this.router.navigateByUrl(`blog?page=${this.currentPage}`);
     return false;
   } 
 
-  selectBlog(blog:Blog, index:number): boolean{
+  // todo
+  clickBlog(blog:Blog, index:number): boolean{
+    // get the next and previous post here using the 'this.blogs[index-1]' and 'this.blogs[index+1]'
+    // but what if clicked blog is the last on page
+    // or user directly access blog post in the URL
+
+    // redirect to blog post page
     this.router.navigateByUrl(`blog/${blog.id}`);
     return false;
   }
